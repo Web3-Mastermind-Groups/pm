2949799b0e1de7439090835ec36291f33aca687b
valmack <8445610+valmack@users.noreply.github.com>
2020-11-22 12:21:04 -0500

    feat: update eslint and truffle config to use dev

--- truffle-config.js
+++ truffle-config.js
@@ -35,7 +35,7 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
   contracts_directory: "./contracts",
-  contracts_build_directory: "./bin",
+  contracts_build_directory: "./src/contracts",
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -44,11 +44,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
